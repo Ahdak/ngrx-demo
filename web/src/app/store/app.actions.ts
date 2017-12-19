91e71ff886ca61f44311e4c59449b9ed89a6f933
pkassem <passam.kassem@gmail.com>
2017-12-20 00:28:26 +0100

    demo ngrx with backend

--- web/src/app/store/app.actions.ts
+++ web/src/app/store/app.actions.ts
@@ -5,7 +5,7 @@ import {User} from '../user';
 export const ADD_USER = 'ADD_USER';
 export const LOAD_USERS = 'LOAD_USERS';
 export const LOAD_USERS_COMPLETED = 'LOAD_USERS_COMPLETED';
-export const SAVE_USERS = 'LOAD_USERS';
+export const SAVE_USERS = 'SAVE_USERS';
 
 export class AddUserAction implements Action {
   readonly type: string = ADD_USER;
@@ -19,5 +19,5 @@ export class LoadUsersActionCompleted implements Action {
   constructor(public payload: User[]) {}
 }
 export class SaveUsersAction implements Action {
-  readonly type: string = LOAD_USERS;
+  readonly type: string = SAVE_USERS;
 }
